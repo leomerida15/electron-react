63d78aefe9b2c37eabd301dfe67e122f1da57516
Dimas Leopoldo Mérida Flores <48163017+leomerida15@users.noreply.github.com>
2021-11-12 20:39:21 -0400

    db estable

--- public/Api/db/keys/one_one.js
+++ public/Api/db/keys/one_one.js
@@ -1,5 +1,5 @@
 module.exports = (model) => {
-	const { Users, Imgs } = model;
+	// const { Users, Imgs } = model;
 	// usuarios at Roles_has_usuarios
 	// Imgs.hasOne(Users, { foreignKey: 'id_img' });
 };
